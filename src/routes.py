3f8419abee4fc7432fbe44eec4717a66dcf6590b
Darius Stefan <darius.stefan@opensips.org>
2025-05-17 18:58:03 +0300

    fix login issues

--- src/routes.py
+++ src/routes.py
@@ -5,8 +5,19 @@ from datetime import datetime, timedelta
 from .schema import UserRegister, UserLogin
 import httpx
 import os
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.security import OAuth2PasswordRequestForm
+from fastapi import Depends
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 db_url = "http://localhost"
 db_port = 8000
@@ -36,22 +47,26 @@ async def register(user: UserRegister):
             raise HTTPException(status_code=400, detail=response.text)
         return {"msg": "User registered"}
 
+
 @app.post("/login")
-async def login(user: UserLogin):
+async def login(form_data: OAuth2PasswordRequestForm = Depends()):
+    username = form_data.username
+    password = form_data.password
     async with httpx.AsyncClient() as client:
-        response = await client.get(f"{db_url}:{db_port}/users/{user.username}/password")
+
+        response = await client.get(f"{db_url}:{db_port}/users/{username}/password")
         if response.status_code != 200:
             raise HTTPException(status_code=400, detail="User not found")
         user_data = response.json()
         hashed_pw = user_data.get("password")
         print(f"Hashed password from DB: {hashed_pw}")
-        print(f"Password from request: {user.password}")
-        if not hashed_pw or not bcrypt.verify(user.password, hashed_pw):
+        print(f"Password from request: {password}")
+        if not hashed_pw or not bcrypt.verify(password, hashed_pw):
             raise HTTPException(status_code=401, detail="Invalid credentials")
 
         role = user_data.get("role", "player")
         token = create_access_token(
-            data={"sub": user.username, "role": role},
+            data={"sub": username, "role": role},
             expires_delta=timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
         )
         return {"access_token": token, "token_type": "bearer"}
