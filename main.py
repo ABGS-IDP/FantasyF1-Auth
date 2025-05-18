7734d3ccb379949d851b0f2a7a0deff272668768
Darius Stefan <darius.stefan@opensips.org>
2025-05-18 22:54:10 +0300

    Fix event name

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ import os
 PORT = int(os.getenv("AUTH_PORT", 8002))
 
 if __name__ == "__main__":
-    uvicorn.run("src.routes:app", host="0.0.0.0", port=PORT, reload=True)
+    uvicorn.run("src.routes:app", host="0.0.0.0", port=PORT)
